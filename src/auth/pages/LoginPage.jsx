85afc4079a619965342d3f7258968a2ac10cd3c0
mauroluis <luisvargas.tbd@gmail.com>
2022-07-11 00:15:39 -0300

    Updating NavBar

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const LoginPage = () => {
-  return <h1>PAGINA DE INICIO DE SESION</h1>;
+  const navigate = useNavigate();
+
+  const onLogin = () => {
+    navigate('/marvel', { replace: true });
+  };
+
+  return (
+    <div className='container mt-5'>
+      <h1>Login</h1>
+      <hr />
+      <button className='btn btn-primary' onClick={onLogin}>
+        Login
+      </button>
+    </div>
+  );
 };
