30f87512496643847004b252aa2e4ab6b957b2d0
mauroluis <luisvargas.tbd@gmail.com>
2022-07-21 18:28:04 -0300

    starting cards structure

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -6,5 +6,16 @@ export const HeroCard = ({
   first_appearance,
   characters,
 }) => {
-  return <div>HeroCard</div>;
+  const herourl = `/assets/heroes/${id}.jpg`;
+  return (
+    <div className='col'>
+      <div className='card'>
+        <div className='row no-gutters'>
+          <div className='col-4'>
+            <img src='' className='card-img' alt={superhero} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
