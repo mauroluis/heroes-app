f7a52ef5efde00b04cadbcf49d9de8d112de8e48
Mauro Luis Vargas <89712970+mauro-vargas-gl@users.noreply.github.com>
2022-07-22 14:06:49 -0300

    Hero cards (#7)
    * starting cards structure
    
    * feature :dizzy: Cards are displaying
    
    Co-authored-by: mauroluis <luisvargas.tbd@gmail.com>

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,3 +1,10 @@
+import { Link } from 'react-router-dom';
+
+const CharactersByHero = ({ alter_ego, characters }) => {
+  if (alter_ego === characters) return <></>;
+  return <p>{characters}</p>;
+};
+
 export const HeroCard = ({
   id,
   superhero,
@@ -6,5 +13,29 @@ export const HeroCard = ({
   first_appearance,
   characters,
 }) => {
-  return <div>HeroCard</div>;
+  const heroImg = `/assets/heroes/${id}.jpg`;
+
+  return (
+    <div className='col'>
+      <div className='card'>
+        <div className='row no-gutters'>
+          <div className='col-4'>
+            <img src={heroImg} className='card-img' alt={superhero} />
+          </div>
+          <div className='col-8'>
+            <div className='card-body'>
+              <h5 className='card-title'>{superhero}</h5>
+              <p className='card-text'>{alter_ego}</p>
+              {/* {alter_ego !== characters && <p>{characters}</p>} */}
+              {CharactersByHero}
+              <p className='cardtext'>
+                <small className='text-muted'>{first_appearance}</small>
+              </p>
+              <Link to={`/hero/${id}`}>Más...</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
